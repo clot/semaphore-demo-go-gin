3651a7ce3e288afc7cecffb9afb5f6a9d36b87bf
MadMark <clotjin@gmail.com>
2024-03-16 16:39:24 +0800

    inital commit

---
+++ models.article.go
@@ -0,0 +1,27 @@
+package main
+
+import "errors"
+
+type article struct {
+	ID int
+	Title string
+	Content string
+}
+
+var articleList = []article{
+	{ID: 1, Title: "Article 1", Content: "Article 1 body"},
+	{ID: 2, Title: "Article 2", Content: "Article 2 body"},
+}
+
+func getAllArticles() []article {
+	return articleList
+}
+
+func getArticleByID(id int) (*article, error) {
+	for _, a := range articleList {
+		if a.ID == id {
+			return &a, nil
+		}
+	}
+	return nil, errors.New("article not found")
+}
